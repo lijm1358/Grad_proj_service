6601233b6911b16d7a722584c501e1e7fdb26acb
kCMI113 <cmkang1316@gmail.com>
2024-05-06 13:04:03 +0900

    feat: add utils for frontend

---
+++ frontend/utils.py
@@ -0,0 +1,23 @@
+from io import BytesIO
+
+import requests
+import streamlit as st
+from PIL import Image
+
+
+@st.cache_resource
+def show_img(img_id, img_url):
+    image = Image.open(BytesIO(requests.get(img_url, stream=True).content))
+    st.image(image, caption=img_id, use_column_width=True)
+
+
+@st.cache_resource
+def show_item(item_info):
+    show_img(item_info["item_id"], item_info["image_link"])
+    _, temp, _ = st.columns([0.35, 0.5, 0.3])
+    with temp:
+        with st.popover("info"):
+            st.markdown(f"상품 번호 : {item_info['item_id']}")
+            st.markdown(f"상품 이름 : {item_info['prod_name']}")
+            st.markdown(f"상품 분류 : {item_info['prod_type_name']}")
+            st.markdown(f"상품 설명 : {item_info['detail_desc']}")
